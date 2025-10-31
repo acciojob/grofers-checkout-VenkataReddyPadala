715e224952d9905938a04a52312bf0312b0bf632
VenkataReddyPadala <84061954+VenkataReddyPadala@users.noreply.github.com>
2025-10-31 15:34:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  const prices = document.querySelectorAll(".price");
+  let total = 0;
+  prices.forEach((price) => {
+    total += Number(price.textContent);
+  });
+  const tr = document.createElement("tr");
+  const td = document.createElement("td");
+  td.colSpan = 2;
+  td.textContent = total;
+  tr.appendChild(td);
+  const table = document.querySelector("table");
+  table.appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
